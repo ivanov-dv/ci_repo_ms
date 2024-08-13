c50402b06c55f8e56b9fb5a9604738a9f753b70a
dnb8866 <dnb8866@gmail.com>
2024-08-13 17:11:51 +0700

    fix logs

--- engine.py
+++ engine.py
@@ -1,3 +1,4 @@
+import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -13,14 +14,16 @@ from utils.repositories import Repository
 sql_db = AlchemySqlDb(cfg.SQLALCHEMY_DATABASE_URL, Base)
 repo = Repository(sql_db)
 
+logger = logging.getLogger('uvicorn.error')
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    logger.info('Prepare database')
     await repo.sql_db.prepare()
     await repo.load_users_from_db()
     await repo.load_requests_from_db()
     yield
-    print("Application shutdown")
+    logging.info("Application shutdown")
 
 
 app = FastAPI(lifespan=lifespan)
